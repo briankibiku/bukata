90c402fee0410c253ce2aeb16ba9616bf1410aa8
b2k <kibikukimani2.com>
2020-01-16 12:15:50 +0300

    refactor: refactoring code

--- test/main_widget_test.dart
+++ test/main_widget_test.dart
@@ -6,8 +6,6 @@ void main() {
   Widget makeTestableWidget({Widget child}) {
     return MaterialApp(
       home: child,
-      // routes: {'/LandingPaeg': (context) => LandingPage(),
-      // '/Home': (context) => HomePage(),},
     );
   }
 
@@ -17,15 +15,10 @@ void main() {
     await tester.pumpWidget(MyApp());
     await tester.tap(find.byType(FlatButton));
     await tester.pump();
-    // await tester.enterText(find.byType(TextField), 'admin@mail.com');
     await tester.enterText(find.byKey(Key('emailTextField')), 'admin@mail.com');
     await tester.enterText(find.byKey(Key('passwordTextField')), 'abcdef');
 
-    // var emailLabel = find.text('Enter email');
-    // var passwordLabel = find.text('Enter password');
     var submitButton = find.byType(FlatButton);
-    // expect(emailLabel, findsOneWidget);
-    // expect(passwordLabel, findsOneWidget);
     expect(submitButton, findsNWidgets(2));
     
   });
